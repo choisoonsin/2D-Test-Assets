acdd2edb647c95eb172ffe18a636690e2e6c5eaf
choisoonsin <cyh0214@gmail.com>
2019-01-05 14:03:53 +0900

    Initial commit

---
+++ Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+    public float attackRange = 5f;
+
+    public float attackRate = 1f;
+    float currentTime = 0f;
+    float time = 0f;
+
+    public float enemySpeed = 0.5f;
+    public Image enemyHealthBar;
+
+    Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        time = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, player.position) <= attackRange)
+        {
+            AttackPlayer();
+        }
+        else
+        {
+            //transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, player.position, enemySpeed * Time.deltaTime);
+        }
+    }
+
+    void AttackPlayer()
+    {
+
+        time -= Time.deltaTime;
+        time = time / attackRate * 10;
+        Debug.Log("Time:" + time);
+
+        //currentTime = Time.deltaTime;
+        if (Time.time > currentTime)
+        {
+            //Debug.Log("Enemy is attaking!!");
+            
+            currentTime = Time.time + attackRate;
+        }
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    float horizontalPos = 0f;
+    float verticalPos = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalPos += Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        verticalPos += Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+        transform.position = new Vector3(horizontalPos, verticalPos, 0f);
+
+    }
+}
